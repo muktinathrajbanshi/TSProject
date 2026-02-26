d883b820a615982e0a712992115a9592d42c5b92
unknown <muktinathrajbanshi786@gmail.com>
2026-02-26 21:43:59 +0545

    mamu kabhi training lene aana

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+const buttonElem = document.querySelector(".clickMe") as HTMLButtonElement;
+const bodyElem: HTMLElement = document.body;
+const isWhite: boolean = false;
+
+
+buttonElem.addEventListener("click", (): void => {
+    if (isWhite) {
+        bodyElem.style.backgroundColor = "";
+    } else {
+        bodyElem.style.backgroundColor = "#CEDEBD";
+    }
+})
+
 console.log("Hello, I am working");
+
